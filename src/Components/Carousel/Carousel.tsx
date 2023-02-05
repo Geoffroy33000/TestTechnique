f025956ef1c0596bd8858b951152cfc867ac7747
Geoffroy Pailhe-belair <geoffroy.pailhebelair@elwin.fr>
2023-02-05 20:15:59 +0100

    Feature/CarouselFromScratch: create and finish a carousel from scratch, add conditions to avoid overscroll, starting using mixin and variables from sass

--- src/Components/Carousel/Carousel.tsx
+++ src/Components/Carousel/Carousel.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 import { CarouselItem } from "../../Components";
+
+import { PartialArrowLeft, PartialArrowRight } from "../../Assets/Icons";
 
 import { ChampagneMock } from "../../Assets/Mocks/ChampagneMock";
 
@@ -10,16 +14,78 @@ interface Props {
 
 function Carousel(props: Props) {
   //const Mock = ChampagneMock;
+  const [principalIndex, setPrincipalIndex] = useState<number>(0);
+
+  console.log(props.drinks.length - 1, principalIndex);
+
   return (
     <div className="Carousel_container">
-      {props.drinks.map((drink: any) => (
+      {principalIndex >= 1 && (
+        <div
+          className="Carousel_control_left"
+          onClick={() => setPrincipalIndex(principalIndex - 1)}
+        >
+          <PartialArrowLeft color="grey" height="50" width="30" />
+        </div>
+      )}
+
+      <div className="Carousel_principal_content">
         <CarouselItem
-          id={drink.idDrink}
-          title={drink.strDrink}
-          image={drink.strDrinkThumb}
-          price={drink.idDrink}
+          id={props.drinks[principalIndex].idDrink}
+          title={props.drinks[principalIndex].strDrink}
+          image={props.drinks[principalIndex].strDrinkThumb}
+          price={props.drinks[principalIndex].idDrink}
+          isPrincipal
         />
-      ))}
+      </div>
+      {props.drinks.length - 1 >= principalIndex + 1 && (
+        <div className="Carousel_second_content">
+          <CarouselItem
+            id={props.drinks[principalIndex + 1].idDrink}
+            title={props.drinks[principalIndex + 1].strDrink}
+            image={props.drinks[principalIndex + 1].strDrinkThumb}
+            price={props.drinks[principalIndex + 1].idDrink}
+          />
+        </div>
+      )}
+      {props.drinks.length - 1 >= principalIndex + 2 && (
+        <div className="Carousel_third_content">
+          <CarouselItem
+            id={props.drinks[principalIndex + 2].idDrink}
+            title={props.drinks[principalIndex + 2].strDrink}
+            image={props.drinks[principalIndex + 2].strDrinkThumb}
+            price={props.drinks[principalIndex + 2].idDrink}
+          />
+        </div>
+      )}
+      {props.drinks.length - 1 >= principalIndex + 3 && (
+        <div className="Carousel_third_content">
+          <CarouselItem
+            id={props.drinks[principalIndex + 3].idDrink}
+            title={props.drinks[principalIndex + 3].strDrink}
+            image={props.drinks[principalIndex + 3].strDrinkThumb}
+            price={props.drinks[principalIndex + 3].idDrink}
+          />
+        </div>
+      )}
+      {props.drinks.length - 1 >= principalIndex + 4 && (
+        <div className="Carousel_third_content">
+          <CarouselItem
+            id={props.drinks[principalIndex + 4].idDrink}
+            title={props.drinks[principalIndex + 4].strDrink}
+            image={props.drinks[principalIndex + 4].strDrinkThumb}
+            price={props.drinks[principalIndex + 4].idDrink}
+          />
+        </div>
+      )}
+      {principalIndex < props.drinks.length - 1 && (
+        <div
+          className="Carousel_control_right"
+          onClick={() => setPrincipalIndex(principalIndex + 1)}
+        >
+          <PartialArrowRight color="grey" height="50" width="30" />
+        </div>
+      )}
     </div>
   );
 }
--- src/Components/CarouselItem/CarouselItem.tsx
+++ src/Components/CarouselItem/CarouselItem.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import classNames from "classnames";
 
 import { Heart } from "../../Assets/Icons";
 
@@ -9,18 +10,30 @@ interface Props {
   title: string;
   image: string;
   price: string;
+  isPrincipal?: boolean;
 }
 
 function CarouselItem(props: Props) {
+  const classes = classNames("CarouselItem_container", {
+    "CarouselItem--principal": props.isPrincipal,
+    "CarouselItem--second": !props.isPrincipal,
+  });
+
   return (
-    <div className="CarouselItem_container">
+    <div className={classes}>
       <Link to={`/details/${props.id}`}>
         <div className="CarouselItem_content">
           <div className="CarouselItem_header">
             <div className="CarouselItem_title">{props.title}</div>
-            <div className="CarouselItem_favorite">
-              <Heart color="white" height="50" width="50" />
-            </div>
+            {props.isPrincipal ? (
+              <div className="CarouselItem_favorite">
+                <Heart color="white" height="50" width="50" />
+              </div>
+            ) : (
+              <div className="CarouselItem_favorite">
+                <Heart color="white" height="30" width="30" />
+              </div>
+            )}
           </div>
           <div className="CarouselItem_price">€ {props.price}</div>
           <div className="CarouselItem_image">
@@ -31,5 +44,9 @@ function CarouselItem(props: Props) {
     </div>
   );
 }
+
+CarouselItem.defaultProps = {
+  isPrincipal: false,
+};
 
 export default CarouselItem;
